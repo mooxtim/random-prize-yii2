13d5ba22925decd2dfced246b357fa3d8570cc09
Tim <antipin@atdev.net>
2018-02-26 12:27:46 +0300

    9 commit

--- frontend/views/random/prize.php
+++ frontend/views/random/prize.php
@@ -14,11 +14,11 @@ use \yii\bootstrap\Html;
 <?php } else if ($prize->type == 'money') { ?>
 <h3>€<?= $prize->count ?>!</h3>
 <p><?= Html::a('Refuse', ['random/refuse', 'id' => $prize->prize_id], ['class' => 'btn btn-danger']) ?></p>
-<p><?= Html::a('Take Money', ['random/take', 'id' => $prize->prize_id], ['class' => 'btn btn-primary']) ?></p>
+<p><?= Html::a('Take the Money', ['random/take', 'id' => $prize->prize_id], ['class' => 'btn btn-primary']) ?></p>
 <p><?= Html::a('Convert to Points', ['random/convert', 'id' => $prize->prize_id], ['class' => 'btn btn-success']) ?></p>
 
 <?php } else { ?>
 <h3>The <?= $prize->name ?>!</h3>
 <p><?= Html::a('Refuse', ['random/refuse', 'id' => $prize->prize_id], ['class' => 'btn btn-danger']) ?></p>
-<p><?= Html::a('Take ' . $prize->name, ['random/take', 'id' => $prize->prize_id], ['class' => 'btn btn-primary']) ?></p>
+<p><?= Html::a('Take the ' . $prize->name, ['random/take', 'id' => $prize->prize_id], ['class' => 'btn btn-primary']) ?></p>
 <?php } ?>
